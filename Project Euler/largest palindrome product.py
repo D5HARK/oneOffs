088163f07d421cda8843b2bec4796e42ad82bb29
D5HARK <devon.sundy@gmail.com>
2025-04-06 11:58:35 -0700

    fixed script, added check for largest rather than first palindrome

--- Project Euler/largest palindrome product.py
+++ Project Euler/largest palindrome product.py
@@ -1,10 +1,11 @@
 def palindrome():
+    largest = 0
     for a in range(999, 99, -1):
         for b in range(999, 99, -1):
             n = a * b
-            if str(n) == str(n)[::-1]:
-                print(n)
-                return
+            if str(n) == str(n)[::-1] and n > largest:
+                largest = n
+    print(largest)
 def main():
     palindrome()
 
